a9ae7360bf5456cf7bceb057ac18412bfe7b028a
Justin Loveless <justin.loveless@outsystems.com>
2025-06-10 14:10:24 -0500

    Refactor NeotroPage layout for consistent header display
    - Removed mobile condition for AppHeader to ensure it is always displayed.
    - Improved overall layout consistency across different screen sizes.

--- src/pages/NeotroPage.tsx
+++ src/pages/NeotroPage.tsx
@@ -28,7 +28,7 @@ const NeotroPage = () => {
 
   return (
     <div className="h-screen w-screen flex flex-col pt-16 md:pt-0">
-      {!isMobile && <AppHeader variant='back' />}
+       <AppHeader variant='back' />
       <div className="flex-1 min-h-0">
         <PokerTable
           session={session}
