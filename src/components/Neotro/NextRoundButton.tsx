9c8cc5baa3e43f6219aa07b229f3bf574aa343f2
Kimberly McIntyre <93159903+mcintyre-ionic@users.noreply.github.com>
2025-06-23 08:58:29 -0400

    chore: polish point selector component and buttons (#40)

--- src/components/Neotro/NextRoundButton.tsx
+++ src/components/Neotro/NextRoundButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface PlayHandButtonProps {
   onHandPlayed: () => void;
@@ -9,13 +9,28 @@ const NextRoundButton: React.FC<PlayHandButtonProps> = ({
   onHandPlayed,
   isHandPlayed,
 }) => {
-  const colorClass = isHandPlayed ? "bg-purple-600 hover:bg-purple-700" : "bg-gray-500";
+  const colorClass = isHandPlayed
+    ? "bg-purple-600 hover:bg-purple-700"
+    : "bg-gray-500";
+  const [isPressed, setIsPressed] = useState(false);
 
   return (
     <button
       onClick={onHandPlayed}
       disabled={!isHandPlayed}
-      className={`grow pl-[6px] rounded-lg ${colorClass} disabled:opacity-50 disabled:cursor-not-allowed`}
+      className={`grow pl-[6px] rounded-lg ${colorClass} disabled:opacity-50 disabled:cursor-not-allowed ${
+        !isHandPlayed
+          ? `${
+              isPressed
+                ? `translate-y-[8px]`
+                : `shadow-[0px_8px_rgba(70,70,90,255)]`
+            }`
+          : `${
+              isPressed
+                ? `translate-y-[8px]`
+                : `shadow-[0px_8px_rgba(80,30,145,255)]`
+            }`
+      }`}
     >
       <div className="font-neotro text-white text-2xl normal-case">
         <span>
